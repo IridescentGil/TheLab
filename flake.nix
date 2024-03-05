27fa2a6dfad94f50fde8fab1e54472454c517d4e
Apile Tyumre <apiletyumre@iridescentsun.com>
2024-03-06 00:21:57 +0200

    Add nix develop flake, edit .gitignore

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+    description = "The Lab workout tracking program made with QT";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = {self, nixpkgs, flake-utils, ...}:
+    flake-utils.lib.eachSystem [
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+        "x86_64-windows"
+        "i686-windows"
+    ] (system:
+    {
+        devShells.default = let
+            pkgs = import nixpkgs {
+                inherit system;
+            };
+        in
+        pkgs.mkShell {
+            packages = with pkgs; [
+                qt6.full
+                qtcreator
+                cmake
+                gnumake
+                gtest
+            ];
+        };
+    });
+}
