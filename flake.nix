96b5128baca55388b09f0887608b334cad2d69e6
Apile Tyumre <apiletyumre@iridescentsun.com>
2024-09-02 03:01:07 +0200

    Nix: Improve nix configuration
    Add nix package build. Remove unnecessary flake config options. Improve nix flake for Qt development. Add Lab library to cmake install option. Format CMakeLists.txt's

--- flake.nix
+++ flake.nix
@@ -22,49 +22,58 @@
         "x86_64-windows"
         "i686-windows"
       ]
-      (system: {
-        devShells.default =
-          let
-            pkgs = import nixpkgs { inherit system; };
-          in
-          pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
-            packages = with pkgs; [
-              kdePackages.full
-              qtcreator
-              cmake
-              gnumake
-              gtest
-              clang-tools
+      (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          packages.default = pkgs.qt6Packages.callPackage pkgs.stdenv.mkDerivation {
+            pname = "TheLab";
+            version = "0.1";
+
+            # The QtQuick project we created with Qt Creator's project wizard is here
+            src = ./.;
+
+            buildInputs = with pkgs; [
               sqlite
               sqlitecpp
+              qt6Packages.full
+              qt6Packages.qtbase
+            ];
+
+            nativeBuildInputs = with pkgs; [
+              cmake
+              qt6Packages.wrapQtAppsHook
+            ];
+
+          };
+          devShells.default = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+            inputsFrom = [ self.packages.${system}.default ];
+            packages = with pkgs; [
+              qtcreator
+              cmake
+              gtest
+              clang-tools
 
               doxygen
               graphviz
 
-              lcov
               llvmPackages.bintools-unwrapped
+              # this is for the shellhook portion
+              qt6.wrapQtAppsHook
+              makeWrapper
+              bashInteractive
             ];
+            # set the environment variables that unpatched Qt apps expect
+            shellHook = ''
+              setQtEnvironment=$(mktemp)
+              random=$(openssl rand -base64 20 | sed "s/[^a-zA-Z0-9]//g")
+              makeWrapper "$(type -p sh)" "$setQtEnvironment" "''${qtWrapperArgs[@]}" --argv0 "$random"
+              sed "/$random/d" -i "$setQtEnvironment"
+              source "$setQtEnvironment"
+            '';
           };
-        devShells.gcc =
-          let
-            pkgs = import nixpkgs { inherit system; };
-          in
-          pkgs.mkShell.override { stdenv = pkgs.gccStdenv; } {
-            packages = with pkgs; [
-              kdePackages.full
-              qtcreator
-              cmake
-              gnumake
-              gtest
-              clang-tools
-              sqlite
-              sqlitecpp
-
-              doxygen
-              graphviz
-
-              lcov
-            ];
-          };
-      });
+        }
+      );
 }
